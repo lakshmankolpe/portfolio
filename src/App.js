6962c341469e4eff8541f1df500393e6091c026a
lakshmankolpe <lakshmankolpe912@gmail.com>
2025-04-11 12:50:38 +0530

    skills section add

--- src/App.js
+++ src/App.js
@@ -3,11 +3,13 @@ import Information from "./components/Information/Information";
 import Project from "./components/Project/Project";
 import Message from "./components/Message/Message";
 import MoreProject from "./components/MoreProject";
+import Skill from "./components/Skills/Skill";
 
 // Layout Component for Home Page
 const Layout = () => (
   <div>
     <Information />
+    <Skill/>
     <Project />
     <Message />
   </div>
---
+++ src/components/Skills/Skill.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import "./Skill.css"
+
+function Skill() {
+  return (
+    <div className='skill-main-container'>
+   <h2>Skills  Sections</h2>
+
+   <div className='sub-container'>
+    <h3>Frontend</h3>
+   </div>
+
+   <div className='sub-container'>
+    <h3>Backend</h3>
+   </div>
+
+   <div className='sub-container'>
+    <h3>Tools</h3>
+   </div>
+
+    
+      </div>
+  )
+}
+
+export default Skill
